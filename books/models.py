e937d4d4a2040e53ef1e533375a9a595200f2520
Akshay Gade <gadeakshay0609@gmail.com>
2024-10-15 12:29:27 +0530

    Database Updated SQLite to MySQL

--- books/models.py
+++ books/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)  # Title of the book
-    author = models.CharField(max_length=100)  # Author's name
-    description = models.TextField()  # Description of the book
-    published_date = models.DateField()  # Publication date
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
 
     def __str__(self):
         return self.title
--- myproject/settings.py
+++ myproject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # Database engine for MySQL
+        'NAME': 'django_crud_db',              # The name of the database you created
+        'USER': 'root',                        # MySQL username
+        'PASSWORD': 'Patil@123',           # MySQL password (use the root password or any other user you've set up)
+        'HOST': 'localhost',                   # Or the IP of your MySQL server
+        'PORT': '3306',                        # Default MySQL port
     }
 }
 
